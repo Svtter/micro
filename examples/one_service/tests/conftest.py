534b89649a0aa9d91aca4bf795a5c72d0d81376f
svtter <svtter@qq.com>
2021-11-27 14:53:02 +0800

    docs: add design pattern

--- examples/one_service/tests/conftest.py
+++ examples/one_service/tests/conftest.py
@@ -2,6 +2,16 @@ import pytest
 from ..conf import Conf
 
 
+"""
+提供两种 fixure 或者 TestCase
+分别用于启动实例测试和非启动实例测试
+
+Client 本身也可以用于常规应用
+
+用于记录请求，记录运行状态，日志，等等。
+"""
+
+
 @pytest.fixture
 def work_service_cls():
     return Conf.service_list[0]
